64fe83f9d422d8f68e3c66519df3354e53e2ba40
Dubinetski <dubinetski@gmail.com>
2017-03-05 21:13:55 +0200

    Раскрытие списка

---
+++ DZ1.py
@@ -0,0 +1,14 @@
+﻿# Раскрыть список [1, 2, 11, 5, [45, [4566, [5]], [[4]], [6]]](сделать его "плоским"):
+
+givenList = [1, 2, 11, 5, [45, [4566, [5]], [[4]], [6]]]
+print(givenList)
+
+def planeList(List, rezult = []):
+    for i in List:
+        if type(i) == list:
+            planeList(i)
+        else:
+            rezult.append(i)
+    return rezult
+
+print(planeList(givenList))
